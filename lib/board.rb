c5847036d7abda04a7cd5b3c9a0f459f09c3fea7
Travis Clarkson <tk1667@gmail.com>
2018-03-03 21:34:37 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -3,6 +3,42 @@ class Board
   attr_accessor
 
   def initialize
+    reset!
+  end
+
+  def cells
+
+  end
+
+  def reset!
+
+  end
+
+  def display
+
+  end
+
+  def position
+
+  end
+
+  def full?
+
+  end
+
+  def turn_count
+
+  end
+
+  def taken?
+
+  end
+
+  def valid_move?
+
+  end
+
+  def update?
 
   end
 
